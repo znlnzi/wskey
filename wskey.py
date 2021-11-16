73f61a3a8bddcb6ca546bc12c80c1adcc9bd2cbb
znlnzi <asura345@gmail.com>
2021-11-16 08:58:13 +0800

    Update wskey.py

--- wskey.py
+++ wskey.py
@@ -240,7 +240,7 @@ def get_sign():
         "uuid": uuid_str,
         "functionId": "genToken",
         "version": "10.0.8",
-        "body": "%7B%22action%22%3A%22to%22%2C%22to%22%3A%22https%253A%252F%252Fplogin.m.jd.com%252Fcgi-bin%252Fm%252Fthirdapp_auth_page%253Ftoken%253DAAEAIEijIw6wxF2s3bNKF0bmGsI8xfw6hkQT6Ui2QVP7z1Xg%2526client_type%253Dandroid%2526appid%253D879%2526appup_type%253D1%22%7D&"
+        "body": '{"action":"to","to":"https%3A%2F%2Fplogin.m.jd.com%2Fcgi-bin%2Fm%2Fthirdapp_auth_page%3Ftoken%3DAAEAIEijIw6wxF2s3bNKF0bmGsI8xfw6hkQT6Ui2QVP7z1Xg%26client_type%3Dandroid%26appid%3D879%26appup_type%3D1"}'
     }
     payload = json.dumps(payload)
     for i in range(3):
