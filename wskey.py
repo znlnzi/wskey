8cbf254828e852c5829a61549b10c2d2d8fc99d4
znlnzi <asura345@gmail.com>
2021-11-16 09:04:11 +0800

    Update wskey.py

--- wskey.py
+++ wskey.py
@@ -235,7 +235,7 @@ def appjmp(wskey, tokenKey):
 def get_sign():
     uuid_value = uuid.uuid1()
     uuid_str = uuid_value.hex
-    url = "http://111.229.103.238:9999/api/wskey/sign"
+    url = "http://host.docker.internal:9999/api/wskey/sign"
     payload = {
         "uuid": uuid_str,
         "functionId": "genToken",
